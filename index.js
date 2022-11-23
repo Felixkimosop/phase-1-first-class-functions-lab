87c8dbb162b6ba5c256fcea359bc65dd2c59d667
Felix Kipruto <kimosopfelix@gmail.com>
2022-11-23 15:25:15 +0300

    Completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+function returnFirstTwoDrivers(){
+
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(){
+
+    return drivers.slice(-2);
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+function  createFareMultiplier(){
+
+    return createFareMultiplier;
+}
+
+const fareMultiplier = function createFareMultiplier(x){
+
+    return x*2;
+}
+
+function  fareDoubler(x){
+
+    return x*2;
+}
+
+function fareTripler(x){
+    return x*3;
+
+}
+
+function selectDifferentDrivers(){
+
+    return returnFirstTwoDrivers();
+    
+}
+
